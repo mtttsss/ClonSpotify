fa843ac7b6642852cad470514e6540c3b2779689
Matias <matias.pinto5@mail.udp.cl>
2026-09-23 14:08:26 -0300

    se agrego la opcion para marcar el corazon y darle like a una cancion. ademas, se cambiaron los operadores en la busqueda para que sea mas facil de leer

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 document.addEventListener('keyup', (e) => {
     if (e.target.matches('.buscador input')) {
         document.querySelectorAll('.portadas').forEach((portadas) => {
-            portadas.textContent.toLowerCase().includes(e.target.value)
-                ? portadas.classList.remove('filtro')
-                : portadas.classList.add('filtro');
+            if (portadas.textContent.toLowerCase().includes(e.target.value)) {
+                portadas.classList.remove('filtro');
+            } else {
+                portadas.classList.add('filtro');
+            }
         });
     }
 });
 
+
 //esto es para la busqueda
 //solo se puede buscar en minisculas, si se busca en mayusculas no funciona
+
+const botonLike = document.querySelector('.like');
+
+botonLike.addEventListener('click', (e) => {
+    if (botonLike.textContent === '♡') {
+        botonLike.textContent = '♥';
+        botonLike.style.color = '#5e17eb';
+    } else {
+        botonLike.textContent = '♡';
+        botonLike.style.color = '#fff';
+    }
+});
+
+// que se rellene el boton del like al hacer click
